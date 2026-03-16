a1d34cf1b9c0a31433d3293ae099b164f9d933d8
jayemscript <johnmarkpulmano052802@gmail.com>
2026-03-16 16:25:28 +0800

    added WIP warning

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { FiAlertTriangle } from "react-icons/fi";
 
 export default function GamesPage() {
-  return <div>GAMES PAGE</div>;
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <FiAlertTriangle className="text-yellow-500 w-16 h-16 mb-4" />
+        <h1 className="text-2xl font-bold mb-2">Page Under Development</h1>
+        <p className="text-gray-600">
+          Sorry! This section of CHEMVERSE is still being cooked. Check back
+          soon for more chemistry fun.
+        </p>
+      </div>
+    </>
+  );
 }
